60c2bba4c4781bb6b9872a287f1bf72005518bab
Gabslimah <biellima.oliver28@gmail.com>
2025-01-30 19:21:08 -0300

    alteração idioma

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'American English Female', {rate:1.2});
+    responsiveVoice.speak(texto, 'Brasilian Portuguese male', {rate:1.2});
 }
 
 function exibirMensagemInicial() {
